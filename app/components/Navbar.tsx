4bab6e55ed839f64ab6a1aac1643c413428a0860
Hasan-Abdullah-Jan <hasanabdullahjan@gmail.com>
2024-11-01 21:48:43 +0500

    Update Navbar.tsx

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+// Navbar.tsx
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            {/* Your Navbar content */}
+        </nav>
+    );
+};
+
+export default Navbar;
